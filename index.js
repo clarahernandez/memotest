cb60a3bc653ddfd25ec29172e6f5770fce864304
Clara Hernández <clara.herni@gmail.com>
2020-06-04 16:48:47 -0300

    boton reiniciar funciona
    Soluciona problema de que al reiniciar los colores se mantenían provocando que se repitan.

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-const COLORES_DISPONIBLES = ['rojo', 'verde', 'amarillo', 'azul', 'negro', 'violeta'];
-let paresColores = [];
-duplicarColores();
+const COLORES = ['rojo', 'verde', 'amarillo', 'azul', 'gris', 'violeta'];
+const LISTA_COLORES = duplicarColores();
 let turnos = 0;
 
 document.querySelector('#boton-empezar').onclick = empezarJuego;
+document.querySelector('#boton-reiniciar').onclick = reiniciarJuego;
 
 function empezarJuego() {
     actualizarEstado('¡Encuentra los pares!');
-    ocultarBotonReiniciar();
-    mostrarBotonEmpezar();
+    ocultarBotonEmpezar();
+    mostrarBotonReiniciar();
     distribuirColores();
+    manejarRonda();
+}
+
+function manejarRonda() {
     desbloquearInputUsuario();
-    manejarInputUsuario();
+    actualizarEstado();
+}
+
+function bloquearCuadro($cuadro) {
+    $cuadro.onclick = function () {};
+}
+
+function manejarInputUsuario(e) {
+    const $cuadro1 = e.target;
+    bloquearCuadro($cuadro1);
+    mostrarOcultarCuadro($cuadro1);
+}
+
+function mostrarOcultarCuadro($cuadro) {
+    $cuadro.classList.toggle('dado-vuelta');
 }
 
 function duplicarColores() {
-    for (let i = 0; i < COLORES_DISPONIBLES.length; i++) {
-        paresColores.push(COLORES_DISPONIBLES[i]);
-        paresColores.push(COLORES_DISPONIBLES[i]);
+    let lista = [];
+    for (let i = 0; i < COLORES.length; i++) {
+        lista.push(COLORES[i]);
+        lista.push(COLORES[i]);
     }
+    return lista;
 }
 
 function distribuirColores() {
-    paresColores = shuffle(paresColores);
+    let listaAux = LISTA_COLORES;
+    listaAux = shuffle(LISTA_COLORES);
     let i = 0;
     document.querySelectorAll('.cuadro').forEach(function ($cuadro) {
-        $cuadro.classList.add(`${paresColores[i]} oculto`);
+        $cuadro.classList.add(`${listaAux[i]}`, 'dado-vuelta');
         i++;
     });
 }
@@ -46,13 +67,14 @@ function ocultarBotonReiniciar() {
     document.querySelector('#boton-reiniciar').className = 'btn btn-outline-danger oculto';
 }
 
-function reiniciar() {
+function reiniciarJuego() {
     bloquearInputUsuario();
     ocultarBotonReiniciar();
     mostrarBotonEmpezar();
     actualizarEstado(
-        'El juego consiste en encontrar los pares de colores.</br>Para empezar dale a jugar.'
+        'El juego consiste en encontrar los pares de colores. Para jugar dale a empezar.'
     );
+    reiniciarCuadros();
     turnos = 0;
 }
 
@@ -88,4 +110,13 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+function reiniciarCuadros() {
+    document.querySelectorAll('.cuadro').forEach(function ($cuadro) {
+        for (let i = 0; i < COLORES.length; i++) {
+            $cuadro.classList.remove(`${COLORES[i]}`);
+        }
+        $cuadro.classList.remove('dado-vuelta');
+    });
 }
